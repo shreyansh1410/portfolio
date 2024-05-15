0791e064dbcdb797269a4e3c693dddb6f696ecf0
Shreyansh Shukla <shreyansh.14010@gmail.com>
2024-05-15 11:36:07 +0530

    udpated import

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 import Parallax from "./components/Parallax/Parallax";
 import Services from "./components/Services/Services";
-import Portfolio from "./components/Portfolio/portfolio";
+import Portfolio from "./components/Portfolio/Portfolio";
 import Contact from "./components/Contact/Contact";
 import Cursor from "./components/Cursor/Cursor";
 
 const App = () => {
   return (
     <div>
-      <Cursor/>
+      <Cursor />
       <section id="Homepage">
         <Navbar />
         <Hero />
@@ -18,12 +18,16 @@ const App = () => {
       <section id="Servies">
         <Parallax type="services" />
       </section>
-      <section><Services/></section>
+      <section>
+        <Services />
+      </section>
       <section id="Portfolio">
         <Parallax type="portfolio" />
       </section>
-      <Portfolio/>
-      <section id="Contact"><Contact/></section>
+      <Portfolio />
+      <section id="Contact">
+        <Contact />
+      </section>
     </div>
   );
 };
